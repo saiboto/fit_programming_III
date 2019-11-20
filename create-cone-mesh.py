16ba4113ea11e1e6ac401d933c5fdc93646d5188
Lenostatos <Lenostatos@gmx.de>
2019-11-21 00:21:02 +0100

    Add some comments and sanitize some names.

--- create-cone-mesh.py
+++ create-cone-mesh.py
@@ -16,14 +16,16 @@ class Mesh:
         self.vertices.append(v2)
         self.vertices.append(v3)
 
+
         self.faces.append(currentIndex + 1)
         self.faces.append(currentIndex + 2)
         self.faces.append(currentIndex + 3)
 
-    def TriangleByIndex(self, v1, v2, v3):
+    def triangleByIndex(self, v1, v2, v3):
         self.faces.append([v1, v2, v3])
 
 ####################### END Mesh class ######################
+
 
 def writeMeshFile(mesh, fileName):
     with open(fileName, "w") as file:
@@ -73,7 +75,7 @@ def createRing(Xrad, Yrad, Nsides, xshift, z):
 # F is the function used to describe the bend.
 # F should always range between 0 and 1 for x between 0 and 1, with f(0) = 0, f(1) = 1
 def f(x):
-    return (4 * (-x ** 2 + x))                  #parabola
+    return (4 * (-x ** 2 + x))                  # parabola
 #Try also:
 #   return (x**4-2*x**3+(5/4)*x**2-(1/4)*x)*(-64)  #polynomial double-bend
 #   return (-math.cos(x*math.pi)+1)/2  #single cos-wave
@@ -81,14 +83,20 @@ def f(x):
 #   return (sqrt(1-(x/2)^2)-cos(1/2))/(1-cos(1/2))  #- 55° circular arc
 
 
+def make_stem(config, n_sides, n_rings):
+    """Creates a stem mesh.
 
-def makeStem(config, Nsides, Nrings):
+    Keyword arguments:
+    config -- StemConfig object ; tree mesh configuration
+    n_sides -- int ; number of sides along the stem. Must be greater than two!
+    n_rings -- int ; number of rings along the stem. Must be greater than two!
+    """
     mesh = Mesh()
 
     # from here: create rings
     rings = []
-    for i in range(0, Nrings + 1):
-        pos = (i / Nrings)
+    for i in range(0, n_rings + 1):
+        pos = (i / n_rings)
         if pos < 0.5:
             xrad = (config.bottomdiam1 * (1 - (2 * pos)) + config.middiam1 * (2 * pos)) / 2
             yrad = (config.bottomdiam2 * (1 - (2 * pos)) + config.middiam2 * (2 * pos)) / 2
@@ -97,7 +105,7 @@ def makeStem(config, Nsides, Nrings):
             yrad = (config.middiam2 * (2 - (2 * pos)) + config.topdiam2 * (2 * (pos - 0.5))) / 2
         xshift = f(pos) * config.bend
         z = pos * config.length
-        r = createRing(xrad, yrad, Nsides, xshift, z)
+        r = createRing(xrad, yrad, n_sides, xshift, z)
         rings.append(r)
     # until here : saved all vertices, organized in rings
     # now: write into mesh-class
@@ -110,30 +118,23 @@ def makeStem(config, Nsides, Nrings):
 
     #Adding triangles for:
     # -front end
-    for i in range(1, Nsides + 1):
-        mesh.TriangleByIndex(i, (i % Nsides) + 1, 0)
+    for i in range(1, n_sides + 1):
+        mesh.triangleByIndex(i, (i % n_sides) + 1, 0)
     # -back end
-    for i in range(1, Nsides + 1):
-        mesh.TriangleByIndex(Nrings*Nsides + i, Nrings*Nsides + (i % Nsides) + 1, (Nrings+1)*Nsides + 1)
+    for i in range(1, n_sides + 1):
+        mesh.triangleByIndex(n_rings * n_sides + i, n_rings * n_sides + (i % n_sides) + 1, (n_rings + 1) * n_sides + 1)
     # -sides
-    for i in range(0, Nrings):
-        for j in range(1, Nsides +1):
-            mesh.TriangleByIndex(i * Nsides + j, (i + 1) * Nsides + j, i * Nsides + ((j % Nsides)+1))
-            mesh.TriangleByIndex((i + 1) * Nsides + ((j % Nsides) +1), (i + 1) * Nsides + j, i * Nsides + ((j% Nsides) +1))
-
-
+    for i in range(0, n_rings):
+        for j in range(1, n_sides + 1):
+            mesh.triangleByIndex(i * n_sides + j, (i + 1) * n_sides + j, i * n_sides + ((j % n_sides) + 1))
+            mesh.triangleByIndex((i + 1) * n_sides + ((j % n_sides) + 1), (i + 1) * n_sides + j, i * n_sides + ((j % n_sides) + 1))
 
     return mesh
 
 
+my_config = StemConfig(30, 2.5, 2.0, 1.8, 1.6, 1.3, 1.3, 20.0)
 
-
-
-
-
-config = StemConfig(30, 2.5, 2.0, 1.8, 1.6, 1.3, 1.3, 20.0)
-
-mesh = makeStem(config, 20, 10)
+mesh = make_stem(my_config, 20, 10)
 
 writeMeshFile(mesh, "mesh.obj")
 
