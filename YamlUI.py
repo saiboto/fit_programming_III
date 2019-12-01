23efbd2427a65fbac70c650c7a75eeea7b4e936a
saiboto <5s2ot@gmx.de>
2019-12-01 21:22:13 +0100

    improved version of the yaml file

--- YamlUI.py
+++ YamlUI.py
@@ -1,9 +1,22 @@
 import yaml
 
-with open('/home/saibot/git_repos/fit_programming_III/simulation_settings.yaml') as file:
-    simulation_settings = yaml.load(file, Loader=yaml.FullLoader)
+def yaml_loader(filepath):
+    with open(filepath, "r") as file_descriptor:
+        data = yaml.load(file_descriptor)
+    return data
 
-    print(simulation_settings)
+def yaml_dump(filepath, data):
+    with open (filepath, "w") as file_descriptor:
+        yaml.dump(data, file_descriptor)
 
-print(simulation_settings['Bottom stem diameter x'])
+if __name__== "__main__":
+    filepath = "/home/saibot/git_repos/fit_programming_III/simulation_settings.yaml"
+    data = yaml_loader(filepath)
+    print data
+    
+    stem_settings = data.get('stem_settings')
+    for stem_settings_name, stem_settings_value in stem_settings.iteritems():
+        print stem_settings_name, stem_settings_value 
+
+
 
