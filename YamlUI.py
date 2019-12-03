20732e8e64d8edbc8eac59bb35a0416fabf7777d
Lenostatos <Lenostatos@gmx.de>
2019-12-03 15:44:07 +0100

    Modify YAML code.

--- YamlUI.py
+++ YamlUI.py
@@ -1,22 +1,25 @@
 import yaml
 
-def yaml_loader(filepath):
-    with open(filepath, "r") as file_descriptor:
-        data = yaml.load(file_descriptor)
-    return data
 
-def yaml_dump(filepath, data):
-    with open (filepath, "w") as file_descriptor:
+def yaml_loader(file_path):
+    with open(file_path, "r") as file_descriptor:
+        # The "FullLoader" is not mentioned in the documentation but there is a
+        # wiki page about it on the package's github:
+        # https://github.com/yaml/pyyaml/wiki/PyYAML-yaml.load%28input%29-Deprecation
+        return yaml.load(file_descriptor, Loader=yaml.FullLoader)
+
+
+def yaml_dump(file_path, data):
+    with open(file_path, "w") as file_descriptor:
         yaml.dump(data, file_descriptor)
 
-if __name__== "__main__":
-    filepath = "/home/saibot/git_repos/fit_programming_III/simulation_settings.yaml"
-    data = yaml_loader(filepath)
-    print data
-    
-    stem_settings = data.get('stem_settings')
-    for stem_settings_name, stem_settings_value in stem_settings.iteritems():
-        print stem_settings_name, stem_settings_value 
 
+if __name__ == "__main__":
+    user_settings_file_path = "simulation_settings.yaml"
+    user_settings = yaml_loader(user_settings_file_path)
+    print(user_settings)
 
+    stem_settings = user_settings.get('stem_settings')
+    for stem_settings_name, stem_settings_value in stem_settings.items():
+        print(stem_settings_name, stem_settings_value)
 
