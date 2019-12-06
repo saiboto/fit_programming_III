5882bed810e59665574a4b40b84f9868953177e3
Lenostatos <Lenostatos@gmx.de>
2019-12-06 14:38:55 +0100

    Write some code to test new functionality.

--- main.py
+++ main.py
@@ -6,7 +6,18 @@ import pybullet_data
 
 import Config
 import Stem
+import YamlUI
+import UserInterface
+import StemConfigFactory
 
+
+user_input = YamlUI.load_user_input('simulation_settings.yaml')
+user_input_validator = UserInterface.Validator(user_input=user_input)
+
+if not user_input_validator.is_valid():
+    user_input_validator.print_reasons()
+
+stem_configs = StemConfigFactory.run(user_input.random_stem_generation)
 
 physicsClient = p.connect(p.GUI)  # or p.DIRECT for non-graphical version
 # necessary for using objects of pybullet_data
@@ -16,43 +27,36 @@ p.setGravity(0, 0, -10)
 
 planeId = p.loadURDF("plane.urdf")
 
-# boxId = p.loadURDF("r2d2.urdf", stem_start_position, stem_start_orientation)
-# boxId = p.loadURDF("random_urdfs/000/000.urdf", stem_start_position, stem_start_orientation)
-
-# my_config = create_cone_mesh.StemConfig(3, 0.25, 0.2, 0.18, 0.16, 0.13, 0.13, bend=0.2)
-# meshes = create_cone_mesh.make_stem(my_config, 20, 10)
-
-my_single_stem_config = Config.SingleStem(length=3,
-                                          bottom_diameter_x=0.25,
-                                          bottom_diameter_y=0.2,
-                                          middle_diameter_x=0.18,
-                                          middle_diameter_y=0.16,
-                                          top_diameter_x=0.13,
-                                          top_diameter_y=0.13,
-                                          bend=0.2)
-
 my_placement = Stem.Placement([0, 0, 2.5], [0, -math.pi * 0.5, 0])
 
 debug_text_id = p.addUserDebugText('', my_placement.position)
 
+a = stem_configs[0]
+
 my_stems = []
-for i in range(500000):
-    if i % 200 == 0:
-        my_stems.append(Stem.Stem(my_single_stem_config, my_placement))
-        debug_text_id = p.addUserDebugText(
-            str(my_stems[-1]._pybullet_id),
-            my_placement.position,
-            replaceItemUniqueId=debug_text_id)
+for stem_config in stem_configs:
+    my_stems.append(Stem.Stem(stem_config, placement=my_placement))
 
-    if i == 1150:
-        p.resetBasePositionAndOrientation(my_stems[5]._pybullet_id,
-                                          my_placement.position,
-                                          my_placement.orientation)
+    for i in range(200):
+        p.stepSimulation()
+        time.sleep(1/100)
 
-    p.stepSimulation()
-    time.sleep(1/100)
-
-# cubePos, cubeOrn = p.getBasePositionAndOrientation(stem_body_id)
-# print(cubePos, cubeOrn)
+#
+# my_stems = []
+# for i in range(500000):
+#     if i % 200 == 0:
+#         my_stems.append(Stem.Stem(my_single_stem_config, my_placement))
+#         debug_text_id = p.addUserDebugText(
+#             str(my_stems[-1]._pybullet_id),
+#             my_placement.position,
+#             replaceItemUniqueId=debug_text_id)
+#
+#     if i == 1150:
+#         p.resetBasePositionAndOrientation(my_stems[5]._pybullet_id,
+#                                           my_placement.position,
+#                                           my_placement.orientation)
+#
+#     p.stepSimulation()
+#     time.sleep(1/100)
 
 p.disconnect()
